16bb85946e2f8007b0e5b6be2446383caf926a79
mitej23 <mitejmadan@gmail.com>
2022-04-20 18:06:36 +0530

    bug: solved some mobile design bugs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <a href="{{ route('dashboard') }}">
             Dashboard
         </a> --}}
-        <h1 style="font-size: 3rem;font-weight: 900;width: 70%;text-align: center;">Access 1000's of Books from our Library</h1>
+        <h1 style="font-size: 3rem;font-weight: 900;width: 70%;text-align: center;" id="headline">Access 1000's of Books from our Library</h1>
         <div class="home-btns-container" style="margin-top: 1rem;">
            {{-- <div class="auth-btn" style="background-color: var(--color-secondary);">
                <a style="text-decoration: none;color: white;font-weight:500" href="{{route('register')}}">Register</a>
